c26df49037eb279e391e0a73ab5143a9c849d9f5
brandonRivera03 <brandon.rivera03@gmail.com>
2016-05-06 11:22:26 -0500

    Added new tests to check functionality
    Created short objects which we will call in a separate file to run these tests. Feel free to add any test. Just comment what the expected output should be so I can add them to the test checker file

--- cmmTest.scala
+++ cmmTest.scala
@@ -1,46 +1,81 @@
-
-
 package cmm
 
-object cmmTest extends Cminusminus {
+object test_Print_String extends Cminusminus {
   def main(args: Array[String]): Unit = {
     Begin
-    Print("Welcome to the guessing game!")
-    Print("Each turn you can guess a number between 1 and 20.")
-    Print("We will give you hints regarding your guess.")
-    Print("--------------")
-    Variable('number) is between(1, 20)
-    Variable('attempts) is 1 
-    Variable ('array) is Array("abc","dsafsd","asdf")
-    Print('array index 0+1)
-    Print(1)
-    While(true)
-      Print("Guess a number between 1 and 20 (inclusive)")
-      Input('guess)
-      If(('guess isLessThan 'number) or ('guess isGreaterThan 'number))
-        If('guess isLessThan 'number)
-          Print ("Higher!")
-        Else
-          Print ("Lower!")
-        EndIf
-      Else
-        Print ("RIGHT!")
-        Break
-      EndIf
-      Print ('number)
-//      If('guess isLessThan 'number)
-//      	Print ("Higher!")
-//      EndIf
-//      If('guess isGreaterThan 'number)
-//      	Print("Lower!")
-//      EndIf
-//      If('guess isEqualTo 'number)
-//      	Print("You got it!")
-//      	Break
-//      EndIf
-      Variable ('attempts) is ('attempts plus 1)
-    Done
-//    Print("It took you", 'attempts, "turns!")
+    Print("hello world")
+    End
+  }
+}
+
+object test_Print_Values extends Cminusminus {
+  def main(args: Array[String]): Unit = {
+    Begin
+    Print(100)
+    Print(333.33)
+    End
+  }
+}
+
+object test_Error extends Cminusminus {
+  def main(args: Array[String]): Unit = {
+    Begin
+    Print("Running Error test...")
+    Error("Encountered string error!")
+    Error(404)
+    Error(40.4)
+    End
+  }
+}
+
+object test_Simple_Assign_Ints extends Cminusminus {
+  def main(args: Array[String]): Unit = {
+    Begin
+    Variable('x) is 1
+    Print('x)
+    Variable('y) equals 10
+    Print('y)
+    Variable('z) isDefinedAs 100
+    Print('z)
+    End
+  }
+}
+
+object test_Simple_Assign_Doubles extends Cminusminus {
+  def main(args: Array[String]): Unit = {
+    Begin
+    Variable('x) is 1.02
+    Print('x)
+    Variable('y) equals 10.03
+    Print('y)
+    Variable('z) isDefinedAs 100.105
+    Print('z)
+    End
+  }
+}
+
+object test_Simple_Assign_String extends Cminusminus {
+  def main(args: Array[String]): Unit = {
+    Begin
+    Variable('x) is "x is"
+    Print('x)
+    Variable('y) equals "y equals"
+    Print('y)
+    Variable('z) isDefinedAs "z isDefinedAs"
+    Print('z)
+    End
+  }
+}
+
+object test_Simple_Assign_List extends Cminusminus {
+  def main(args: Array[String]): Unit = {
+    Begin
+    Variable('x) is List(10,20,30,40)
+    Print('x)
+    Variable('y) equals List("ab", "cd", "efg", "h")
+    Print('y)
+    Variable('z) isDefinedAs List("var", 19, "equals", 19.0)
+    Print('z)
     End
   }
 }
